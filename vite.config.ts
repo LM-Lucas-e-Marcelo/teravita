96f68a77ed212f12544d9099d0f47cf4effba8dd
Lucas Sagás <lucas.sagas@rdstation.com>
2025-07-18 12:26:46 -0300

    remove Netlify configuration and simplify Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig, Plugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  build: {
-    outDir: "dist/spa",
-  },
+export default defineConfig(() => ({
   plugins: [react()],
   resolve: {
     alias: {
