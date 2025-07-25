7ac51142bc6afb94b358b2ce9b3734c0bd14a645
Lucas Sagás <lucassaagas@gmail.com>
2025-07-25 13:25:21 -0300

    add testimonials data

---
+++ src/constants/testimonials.ts
@@ -0,0 +1,23 @@
+export const TESTIMONIALS = [
+  {
+    name: "Luanderson Bezerra",
+    role: "Pai de paciente",
+    content:
+      "Ambiente acolhedor e seguro. Minha filha nunca esteve tão envolvida nas atividades. Obrigada, Teravita! 🙏",
+    rating: 5,
+  },
+  {
+    name: "Gabriel Severo",
+    role: "Pai de paciente",
+    content:
+      "Recomendamos a clínica pra todos os pais que procuram um tratamento carinhoso e profissional para seus filhos. E a localização é muito conveniente, com estacionamento perto.",
+    rating: 5,
+  },
+  {
+    name: "Elisandra Moura",
+    role: "Mãe de paciente",
+    content:
+      "A clínica TeraVita é maravilhosa! Profissionais qualificados e atenciosos",
+    rating: 5,
+  },
+];
