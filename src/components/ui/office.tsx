4f73c8fd1f812d67628c6b64b2310db0dc414c3d
Lucas Sagás <lucassaagas@gmail.com>
2025-07-26 09:31:17 -0300

    add images

--- src/components/ui/office.tsx
+++ src/components/ui/office.tsx
@@ -1,3 +1,14 @@
+import office1 from "@/assets/office/office1.jpg";
+import office2 from "@/assets/office/office2.jpg";
+import office3 from "@/assets/office/office3.jpg";
+import office4 from "@/assets/office/office4.jpg";
+import office5 from "@/assets/office/office5.jpg";
+import office6 from "@/assets/office/office6.jpg";
+import office7 from "@/assets/office/office7.jpg";
+import office8 from "@/assets/office/office8.jpg";
+import office9 from "@/assets/office/office9.jpg";
+import office10 from "@/assets/office/office10.jpg";
+
 export const Office = () => {
   return (
     <section
@@ -20,14 +31,14 @@ export const Office = () => {
           <div className="space-y-4">
             <div className="aspect-square rounded-xl bg-primary/10 overflow-hidden">
               <img
-                src="https://www.psicologo.com.br/wp-content/uploads/terapia-infantil-psicologo.jpg"
+                src={office1}
                 alt="Consultório"
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
               />
             </div>
             <div className="aspect-[4/3] rounded-xl bg-accent/20 overflow-hidden">
               <img
-                src="https://lh7-rt.googleusercontent.com/docsz/AD_4nXdHAufGAQ9yB8nj2yk16dxYYRLz9KjbX4azk_e8ph8iRxBGSOiej4Fi8lQnWv2CrB4VODBPKRcgwPUTSc5zO2sxau7qrysaPbNIjn0M2ZxnA9ZnacKC-BbMYOiarv-SthsiaSLqig?key=BfO5PGwMP_lQDnMw4kIPwV_E"
+                src={office2}
                 alt="Sala de terapia"
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
               />
@@ -36,21 +47,21 @@ export const Office = () => {
           <div className="space-y-4 mt-8">
             <div className="aspect-[4/2] rounded-xl bg-primary/10 overflow-hidden">
               <img
-                src="https://www.psicologo.com.br/wp-content/uploads/terapia-infantil-psicologo.jpg"
+                src={office3}
                 alt="Consultório"
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
               />
             </div>
             <div className="aspect-[4/3] rounded-xl bg-primary/10 overflow-hidden">
               <img
-                src="https://www.psicologo.com.br/wp-content/uploads/terapia-infantil-psicologo.jpg"
+                src={office4}
                 alt="Consultório"
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
               />
             </div>
             <div className="aspect-[4/2] rounded-xl bg-accent/20 overflow-hidden">
               <img
-                src="https://lh7-rt.googleusercontent.com/docsz/AD_4nXdHAufGAQ9yB8nj2yk16dxYYRLz9KjbX4azk_e8ph8iRxBGSOiej4Fi8lQnWv2CrB4VODBPKRcgwPUTSc5zO2sxau7qrysaPbNIjn0M2ZxnA9ZnacKC-BbMYOiarv-SthsiaSLqig?key=BfO5PGwMP_lQDnMw4kIPwV_E"
+                src={office5}
                 alt="Sala de terapia"
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
               />
@@ -59,14 +70,14 @@ export const Office = () => {
           <div className="space-y-4">
             <div className="aspect-[4/3] rounded-xl bg-primary/10 overflow-hidden">
               <img
-                src="https://www.psicologo.com.br/wp-content/uploads/terapia-infantil-psicologo.jpg"
+                src={office6}
                 alt="Consultório"
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
               />
             </div>
             <div className="aspect-[4/4] rounded-xl bg-accent/20 overflow-hidden">
               <img
-                src="https://lh7-rt.googleusercontent.com/docsz/AD_4nXdHAufGAQ9yB8nj2yk16dxYYRLz9KjbX4azk_e8ph8iRxBGSOiej4Fi8lQnWv2CrB4VODBPKRcgwPUTSc5zO2sxau7qrysaPbNIjn0M2ZxnA9ZnacKC-BbMYOiarv-SthsiaSLqig?key=BfO5PGwMP_lQDnMw4kIPwV_E"
+                src={office7}
                 alt="Sala de terapia"
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
               />
@@ -75,21 +86,21 @@ export const Office = () => {
           <div className="space-y-4 mt-8">
             <div className="aspect-[4/2] rounded-xl bg-secondary/20 overflow-hidden">
               <img
-                src="https://www.psicologo.com.br/wp-content/uploads/terapia-infantil-psicologo.jpg"
+                src={office8}
                 alt="Espaço infantil"
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
               />
             </div>
             <div className="aspect-[4/3] rounded-xl bg-secondary/20 overflow-hidden">
               <img
-                src="https://www.psicologo.com.br/wp-content/uploads/terapia-infantil-psicologo.jpg"
+                src={office9}
                 alt="Espaço infantil"
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
               />
             </div>
             <div className="aspect-[4/2] rounded-xl bg-muted/40 overflow-hidden">
               <img
-                src="https://inpaonline.com.br/wp-content/uploads/2024/12/terapia-infantil-1024x597.jpg"
+                src={office10}
                 alt="Recepção"
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
               />
